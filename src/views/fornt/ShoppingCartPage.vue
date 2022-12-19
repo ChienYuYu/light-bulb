77b5649df09c4f08e68365b7805663e072c50084
陳建佑 <jianyou224@gmail.com>
2022-12-19 11:48:59 +0800

    購物車頁切版

--- src/views/fornt/ShoppingCartPage.vue
+++ src/views/fornt/ShoppingCartPage.vue
@@ -3,17 +3,71 @@
     <div class="header">
       <h1>購物車</h1>
     </div>
+
+    <!-- ----------------- -->
+    <div class="main">
+      <div class="row">
+        <div class="col-md-12 col-lg-10 col-xl-6 mx-auto">
+          <ul class="cart-list">
+            <li v-for="item in carts" :key="item.id">
+              <div class="img-title-group col-12 col-md-6">
+                <img :src="item.picture" alt="">
+                <h3 class="title">{{ item.title }}</h3>
+              </div>
+              <!-- ------------------- -->
+              <div class="price-qty-wrap col-12 col-md-5">
+                <!-- ------------------- -->
+                <div class="price-group">
+                  <p class="price">售價: ${{ item.price }}</p>
+                  <p class="price">總價: ${{ item.price }}</p>
+                </div>
+
+                <div class="qty">
+                  <button class="sub">-</button>
+                  <input type="number" class="qty" aria-label="1" v-model="qty">
+                  <button class="add">+</button>
+                </div>
+                <!-- ------------------- -->
+              </div>
+              <button class="btn remove">X</button>
+            </li>
+          </ul>
+          <div class="pay-btn-wrap">
+            <router-link to="/product/全部" class="btn">繼續購物</router-link>
+            <router-link to="/" class="btn">前往結帳</router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- ----------------- -->
   </div>
+  <!-- <Footer></Footer> -->
 </template>
 
 <script>
-export default {
+// import Footer from '@/components/FooterComponent.vue';
+import { ref, computed } from 'vue';
+import { useStore } from 'vuex';
 
+export default {
+  // components: { Footer },
+  setup() {
+    const store = useStore();
+    const carts = computed(() => store.state.shoppingCart.shoppingCart);
+    const qty = ref(1);
+    return { carts, qty };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-  .header{
+.shopping-cart {
+  background: rgb(18, 18, 29);
+  height: 100vh;
+  overflow: auto;
+}
+
+.header {
   background: url(@/assets/img/shoppingCartPage.jpg);
   width: 100%;
   height: 300px;
@@ -29,5 +83,136 @@ export default {
     text-align: center;
     backdrop-filter: blur(3px);
   }
+}
+
+.main {
+  padding: 3rem 2rem;
+}
+
+ul.cart-list {
+  list-style: none;
+  padding: 0;
+
+  li {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border: 1px solid #666;
+    border-radius: .5rem;
+    overflow: hidden;
+    margin-bottom: 1rem;
+
+    @media (max-width: 767px) {
+      flex-direction: column;
+    }
+
+    .img-title-group {
+      display: flex;
+      align-items: center;
+
+      @media (max-width: 767px) {
+        border-bottom: 1px solid #666;
+      }
+
+      img {
+        width: 100px;
+        margin-right: .5rem;
+      }
+
+      h3.title {
+        color: #fff;
+        font-size: 1.3rem;
+      }
+
+    }
+
+    .price-qty-wrap {
+      display: flex;
+      align-items: center;
+
+      @media (max-width: 767px) {
+        border-bottom: 1px solid #666;
+        justify-content: space-between;
+      }
+
+      .price-group {
+        padding: 1rem;
+
+        @media (max-width: 767px) {
+          display: flex;
+        }
+
+        p.price {
+          color: #fff;
+          margin-bottom: 0;
+          padding: 0 .5rem;
+        }
+      }
+
+      .qty {
+        width: 50%;
+        // border: 1px solid #f00;
+
+        input[type=number] {
+          width: 30%;
+          text-align: center;
+          padding: 0;
+          border: 1px solid#eee;
+          background: #eee;
+
+          @media (max-width: 767px) {
+            width: 50%;
+          }
+        }
+
+        button.sub,
+        button.add {
+          background: rgb(255, 211, 77);
+          border: none;
+        }
+      }
+    }
+
+    button.remove {
+      color: rgb(255, 68, 68);
+      padding: .5rem 1rem;
+      margin: .6rem;
+      border: 1px solid rgb(255, 68, 68);
+
+      &:hover {
+        background: rgb(255, 68, 68);
+        color: #fff;
+      }
+    }
+  }
+}
+
+.pay-btn-wrap {
+  padding: 3rem 0;
+  display: flex;
+  justify-content: center;
+
+  a {
+    margin: 0 1rem;
+    background: rgb(255, 211, 77);
+    color: #333;
+
+    &:hover {
+      color: #fff;
+    }
+  }
+}
+
+// input type number 隱藏箭頭
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
 }
 </style>
