1dc68c9541bc2c812362683fb7033a7ba168d430
陳建佑 <jianyou224@gmail.com>
2023-02-05 19:26:19 +0800

    改打包丟到express部署

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,16 @@
 const { defineConfig } = require('@vue/cli-service');
 
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/light-bulb/'
+//     : '/',
+// });
+
+// 打包丟到express用下面這段
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/light-bulb/'
+    ? '/'
     : '/',
 });
